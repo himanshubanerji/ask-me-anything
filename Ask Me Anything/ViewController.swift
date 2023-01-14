183fe641ffef5366962461929d6b441b84b68686
Himanshu Banerji <himanshubanerji@gmail.com>
2023-01-14 21:41:59 +0530

    final changes added

--- Ask Me Anything/ViewController.swift
+++ Ask Me Anything/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var answerPokeball: UIImageView!
+    
+    let pokeballAnswer = [ #imageLiteral(resourceName: "open-pokemon-yes"), #imageLiteral(resourceName: "open-pokemon-no"), #imageLiteral(resourceName: "open-pokemon-try-again"), #imageLiteral(resourceName: "open-pokemon-yes1"), #imageLiteral(resourceName: "open-pokemon-no-idea") ]
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        answerPokeball.image = pokeballAnswer.randomElement()
     }
-
-
+    
+    @IBAction func askbuttonEntered(_ sender: UITextField) {
+        answerPokeball.image = pokeballAnswer.randomElement()
+    }
 }
-
